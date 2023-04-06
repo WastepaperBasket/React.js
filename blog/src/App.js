a19ef970b07e4232869736e93a55a8b1f0f05fdb
pdy1207 <pdy1207@gmail.com>
2023-04-06 22:09:05 +0900

    useState

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let post = "강남 우동 맛집";
-
+  let [title, b] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "여자 신발 추천",
+  ]);
+  let [logo, setLogo] = useState("ReactBlog");
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4> {post} </h4>
+      <div className="list">
+        <h4> {title[0]}</h4>
+        <p>2월17일 발행</p>
+      </div>
+      <div className="list">
+        <h4> {title[1]}</h4>
+        <p>2월17일 발행</p>
+      </div>
+      <div className="list">
+        <h4> {title[2]}</h4>
+        <p>2월17일 발행</p>
+      </div>
     </div>
   );
 }
