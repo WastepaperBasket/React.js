2af9e05021bfabc7340c665b99ae71dc50825190
pdy1207 <pdy1207@gmail.com>
2023-04-08 19:54:28 +0900

    array/object state ... || onClick

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,20 +3,54 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [title, b] = useState([
-    "남자 코트 추천",
+  let [title, settitle] = useState([
+    "남성 코트 추천",
     "강남 우동 맛집",
-    "여자 신발 추천",
+    "남녀노소 신발 추천",
   ]);
   let [logo, setLogo] = useState("ReactBlog");
+  let [like, setlike] = useState(0);
+  function setDate() {
+    let copy = [...title];
+    copy[0] = "여자 코트 추천";
+    settitle(copy);
+  }
   return (
     <div className="App">
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
+
+      <button onClick={setDate}>이름변경</button>
+      <button
+        onClick={() => {
+          let copy = [...title].sort();
+          settitle(copy);
+        }}
+      >
+        가나다순 정렬
+      </button>
       <div className="list">
-        <h4> {title[0]}</h4>
+        <h4>
+          {title[0]}
+          <span
+            onClick={() => {
+              setlike(like++);
+            }}
+          >
+            🫰
+          </span>
+          {like}
+        </h4>
         <p>2월17일 발행</p>
+
+        {/* <button
+          onClick={() => {
+            settitle(["여자 코트 추천", "강남 우동 맛집", "여자 신발 추천"]);
+          }}
+        >
+          이름변경
+        </button> 잘못된 변경법임 */}
       </div>
       <div className="list">
         <h4> {title[1]}</h4>
