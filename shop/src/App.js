a6fc887fdd52840d24746386a8cb2c6da2cc8201
pdy1207 <pdyme1207@gmail.com>
2023-04-20 00:06:48 +0900

    컴포넌트까지 완료 반복문 및 갯수는 다시

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
-
 import bg from "./img/bg.png";
+import { useState } from "react";
+import shoping from "./data.js";
+
 function App() {
+  let [shoes, setshoes] = useState(shoping);
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -24,13 +27,9 @@ function App() {
       <Container>
         <Row>
           <Col>
-            <img
-              className="shop-item"
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-            />
-            <h4>상품명</h4> <p>상품설명</p>
+            <Product />
           </Col>
-          <Col>
+          {/* <Col>
             <img
               className="shop-item"
               src="https://codingapple1.github.io/shop/shoes2.jpg"
@@ -43,11 +42,30 @@ function App() {
               src="https://codingapple1.github.io/shop/shoes2.jpg"
             />{" "}
             <h4>상품명</h4> <p>상품설명</p>
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </div>
   );
+}
+function Product() {
+  let [shoes2, setshoes2] = useState(shoping);
+  {
+    shoes2.map(function (a, i) {
+      return (
+        <>
+          {/* <img
+            className="shop-item"
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+          /> */}
+          <h3>{shoes2[i].id} </h3>
+          <h4>{shoes2[i].title}</h4>
+          <p>{shoes2[i].content}</p>
+          <p>{shoes2[i].price} </p>
+        </>
+      );
+    });
+  }
 }
 
 export default App;
