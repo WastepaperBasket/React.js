0a889e3253001b1a014c00d70fd6d9b27e87d6d6
pdy1207 <pdyme1207@gmail.com>
2023-04-22 16:56:47 +0900

    기본 라우팅 + 셋팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,6 +4,7 @@ import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import bg from "./img/bg.png";
 import { useState } from "react";
 import shoping from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   let [shoes, setshoes] = useState(shoping);
@@ -11,26 +12,70 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React Shop</Navbar.Brand>
+          <Navbar.Brand href="/">React Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to="/">Home</Link>
+            <Link to="datail">Features</Link>
           </Nav>
         </Container>
       </Navbar>
-      <div
-        className="main-bg"
-        style={{ backgroundImage: "url(" + bg + ")" }}
-      ></div>
 
-      <Container>
-        <Row>
-          {shoes.map((a, i) => {
-            return <Product shoes={shoes[i]} i={i} />;
-          })}
-        </Row>
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: "url(" + bg + ")" }}
+              ></div>
+
+              <Container>
+                <Row>
+                  {shoes.map((a, i) => {
+                    return <Product shoes={shoes[i]} i={i} />;
+                  })}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route
+          path="/datail"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: "url(" + bg + ")" }}
+              ></div>
+
+              {shoes.map((a, i) => {
+                return <DatailPro shoes={shoes[i]} i={i} />;
+              })}
+            </>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+function DatailPro(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`}
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
     </div>
   );
 }
