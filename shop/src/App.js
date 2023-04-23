aba8474a7ee26119d6a0186d2abafd0c966d8d8c
pdy1207 <pdyme1207@gmail.com>
2023-04-23 22:30:38 +0900

    페이지 이동기능 ex) admin/member ...

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,18 +4,32 @@ import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import bg from "./img/bg.png";
 import { useState } from "react";
 import shoping from "./data.js";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
   let [shoes, setshoes] = useState(shoping);
+  let navigate = useNavigate();
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">React Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Link to="datail">Features</Link>
+            <Nav.Link
+              onClick={() => {
+                navigate(1);
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/datail");
+              }}
+            >
+              Features
+            </Nav.Link>
+            {/* a 태그와 동일함 */}
           </Nav>
         </Container>
       </Navbar>
@@ -55,10 +69,37 @@ function App() {
             </>
           }
         />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>/about/member입니당</div>} />
+        </Route>
+
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+
+        <Route path="*" element={<div>없는페이지입니다.</div>} />
       </Routes>
     </div>
   );
 }
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트!</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
 function DatailPro(props) {
   return (
     <div className="container">
@@ -71,14 +112,16 @@ function DatailPro(props) {
         </div>
         <div className="col-md-6">
           <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h3>{props.shoes.id} </h3>
+          <h4>{props.shoes.title}</h4>
+          <p>{props.shoes.content}</p>
+          <p>{props.shoes.price} </p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
     </div>
   );
-}
+} // 다른곳에 만들어서 export default DatailPro;  import 잘하면 끝!
 function Product(props) {
   return (
     <div className="col-md-4">
